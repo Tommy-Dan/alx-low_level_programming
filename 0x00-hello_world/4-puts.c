de5440a1c70bf4430f5b45287f04eac43ea20196
Tommy-Dan <ttuckerwra@gmail.com>
2023-06-15 17:55:41 -0700

    C program that prints something

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
- #include<stdio.h>
-  2 /**
-  3   * main - Entry point
-  4   * Return: Return (0)
-  5   */
-  6
-  7 int main(void)
-  8 {
-  9         puts("\"Programming is like building a multilingual puzzle");
- 10         return (0);
- 11 }
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Return (0)
+ */
 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
