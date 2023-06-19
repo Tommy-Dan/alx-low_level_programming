1958330ccabb05235223b4ad16ba16a2beb2cfe8
Tommy-Dan <ttuckerwra@gmail.com>
2023-06-19 15:55:00 -0700

    print the exact words

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  * Description: Program that prints exactly provided text
- * Return: Return (0)
+ * Return: Return (1)
  */
 
 int main(void)
 {
-	 printf("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (0);
 }
