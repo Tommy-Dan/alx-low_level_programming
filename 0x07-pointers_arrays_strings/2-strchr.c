63c533b2c09c336cf0d09bf627d0332b64f1990c
Tommy-Dan <ttuckerwra@gmail.com>
2023-07-04 05:32:54 -0700

    locating a character in a string at it first occurrence and return the rest

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  * _strchr - locates a character in a string
  * @s: the string addres that stores characters
  * @c: the character in the string
- * Return: Return the address string
+ * Return: Return (0)
  *
  */
 
@@ -12,7 +12,7 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i] >= '\0')
 	{
 		if (s[i] == c)
 		return (&s[i]);
