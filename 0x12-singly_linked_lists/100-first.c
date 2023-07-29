40bfec2895bcc7fb5c69384d9309dc099a032f2b
Tommy-Dan <ttuckerwra@gmail.com>
2023-07-28 19:07:49 -0700

    function that prints ... before main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 
+
+void first(void) __attribute__ ((constructor));
+
 /**
  * first - prints a sentence before the main
  * function is executed
  */
-
-
-void first(void) __attribute__ ((constructor));
-
-
 
 void first(void)
 {
